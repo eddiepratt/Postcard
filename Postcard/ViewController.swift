469be28d8b561036bb32d4db2073329afe070643
Eddie Pratt <edwardpratt@gmail.com>
2014-09-13 15:12:56 +0100

    first rev of project

---
+++ Postcard/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Postcard
+//
+//  Created by Edward Pratt on 12/09/2014.
+//  Copyright (c) 2014 Edward Pratt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        messageLabel.sizeToFit()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //Code will evaluate when we press the button
+        
+        messageLabel.text=enterMessageTextField.text
+        enterMessageTextField.text=nil
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        messageLabel.hidden=false
+        enterMessageTextField.resignFirstResponder()
+        
+    }
+
+}
+
